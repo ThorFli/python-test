8ca02a7188d548a1feae846ebfa8dc2105c594ef
Thorsten Flierl <thorstenflierl@me.com>
2023-03-05 11:57:25 +0100

    Erster Commit

---
+++ sonnenaufgang.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import this
+
+this
+
+city = 'Hamburg'
+url = f'https://sonnenaufgang.online/sun/{city}'
+#url = f'https://zwiftpower.com/profile.php?z=4516687'
+## url = f'https://sonnenaufgang.online/sun/larvik'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+#print(response.text)
+
+
+sunrise_data = soup.find('div', {'data-name': 'sunrise'})
+sunrise_data = sunrise_data.text
+sunrise_data = sunrise_data[:-3]
+print(f'Die Sonne geht in {city} heute um {sunrise_data} auf.')
+
